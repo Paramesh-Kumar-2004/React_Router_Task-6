8eb1ec07f951de3cce614dc61c6478199cecdfa1
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-11-03 22:51:29 +0530

    Loading Added In Home Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,6 +6,7 @@ import { getProducts } from '../API/API_Call'
 import Cards from '../Components/Cards'
 import img from "../assets/Obito_5.jpg"
 import img2 from "../assets/Akatsuki_Members_11.jpg"
+import Loader from '../Components/Loader'
 
 
 
@@ -14,7 +15,8 @@ const Home = ({ HandleAdCart, cartItem }) => {
     const navigate = useNavigate()
 
     const [loading, setLoading] = useState(true)
-    
+    const [loadingMessage, setLoadingMessage] = useState("Loading...")
+
     const [products, setProducts] = useState([])
 
     useEffect(() => {
@@ -26,11 +28,21 @@ const Home = ({ HandleAdCart, cartItem }) => {
         try {
             const response = await getProducts()
             setProducts(response)
+            setLoading(!loading)
         }
         catch (error) {
             console.log(error)
         }
     }
+
+    if (loading) {
+        return (
+            <div>
+                <Loader loadingMessage={loadingMessage} />
+            </div>
+        )
+    }
+
 
     return (
         <div>
@@ -39,11 +51,7 @@ const Home = ({ HandleAdCart, cartItem }) => {
                 <button onClick={() => navigate("/CreateProducts")} id="CreateButton">Create Product</button>
             </div>
 
-            <h1 style={{ color: "white" }}>Products</h1>
-
-
             <Cards items={products} HandleAdCart={HandleAdCart} cartItem={cartItem} />
-
 
         </div>
     )
